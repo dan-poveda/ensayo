812755d695a84588f8315eb8e790866bb257560e
dany poveda <karendaniela110210@gmail.com>
2018-05-13 11:17:33 -0500

    first commit

---
+++ primero.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>moodle colombia</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	
+
+</body>
+</html>
